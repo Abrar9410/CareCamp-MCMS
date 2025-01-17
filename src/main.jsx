90d4121d587de6fcde60842ff1e178fa4e44fad8
Abrar Shahriar <abrarshahriar9410@gmail.com>
2025-01-17 20:43:25 +0600

    Firebase Authentication implemented

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,17 @@ import './index.css'
 import { RouterProvider } from "react-router-dom";
 import Router from './routes/Router';
 import { HelmetProvider } from 'react-helmet-async';
+import { ToastContainer } from 'react-toastify';
+import AuthProvider from './providers/AuthProvider';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
-      <RouterProvider router={Router}></RouterProvider>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <RouterProvider router={Router}></RouterProvider>
+      </HelmetProvider>
+    </AuthProvider>
+    <ToastContainer/>
   </StrictMode>,
 )
---
+++ src/pages/AuthPages/UpdateProfile.jsx
@@ -0,0 +1,63 @@
+import { useContext, useState } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+
+
+const UpdateProfile = () => {
+
+    const { user, updateUserProfile, setLoading } = useContext(AuthContext);
+    const [errorMessage, setErrorMessage] = useState('');
+    const navigate = useNavigate();
+
+    const handleUpdateProfile = e => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const photo = e.target.photo.value;
+        setErrorMessage('');
+        const profile = {
+            displayName: name,
+            photoURL: photo
+        };
+        updateUserProfile(profile)
+            .then(() => {
+                setLoading(false);
+                navigate("/user-profile");
+            })
+            .catch(error => setErrorMessage(error.message));
+        toast.success("Profile Updated Successfully!!!", {
+            position: "top-center"
+        });
+    }
+
+    return (
+        <div className="w-10/12 sm:w-2/3 xl:w-1/3 mx-auto border border-base-200 p-8 bg-white rounded-lg shadow-xl my-16">
+            <form onSubmit={handleUpdateProfile} className="px-5 flex flex-col gap-4">
+                <div className="form-control">
+                    <label className="label">
+                        <span className="font-semibold">Edit Username:</span>
+                    </label>
+                    <input
+                        type="text"
+                        name="name"
+                        defaultValue={user && user.displayName}
+                        className="input input-bordered input-md" required />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="font-semibold">Edit Photo URL:</span>
+                    </label>
+                    <input
+                        type="text"
+                        name="photo"
+                        defaultValue={user && user.photoURL}
+                        className="input input-bordered input-md" required />
+                </div>
+                <p className="text-red-600">{errorMessage}</p>
+                <input type="submit" value="Update Profile" className="btn btn-primary" />
+            </form>
+        </div>
+    );
+};
+
+export default UpdateProfile;
