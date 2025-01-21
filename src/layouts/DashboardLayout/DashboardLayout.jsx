56dcabe44f7c73498eb7b21e95d79b540b93b288
Abrar Shahriar <abrarshahriar9410@gmail.com>
2025-01-22 00:58:42 +0600

    Profile page done

--- src/layouts/DashboardLayout/DashboardLayout.jsx
+++ src/layouts/DashboardLayout/DashboardLayout.jsx
@@ -8,21 +8,43 @@ import { FaHouseMedicalCircleCheck } from "react-icons/fa6";
 import { RiHomeGearFill, RiHomeGearLine, RiLogoutCircleLine } from "react-icons/ri";
 import { IoMdAnalytics } from "react-icons/io";
 import { MdAddHome, MdManageAccounts } from "react-icons/md";
+import useAdmin from "../../hooks/useAdmin";
+import useAuth from "../../hooks/useAuth";
+import Swal from "sweetalert2";
 
 
 const DashboardLayout = () => {
+
+    const { isAdmin } = useAdmin();
+    const {logOut} = useAuth();
+
+    const handleLogOut = () => {
+            Swal.fire({
+                title: "Are you sure you want to log out?",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#d33",
+                cancelButtonColor: "#3085d6",
+                confirmButtonText: "Yes, log out!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    logOut();
+                }
+            });
+        }
+
     return (
         <>
             <Helmet><title>CareCamp | Dashboard</title></Helmet>
-            <Link to="/" className="flex items-center absolute left-8 top-2 sm:top-4 z-10">
+            <Link to="/" className="flex items-center absolute left-8 top-2 sm:top-3 z-10 text-primary hover:text-white">
                 <img src={logo} alt="logo" className="w-4 min-[275px]:w-6 min-[400px]:w-8 sm:w-10 lg:w-12 xl:w-14" />
-                <span className="font-poppins text-primary max-[275px]:text-sm min-[400px]:text-lg sm:text-xl lg:text-2xl xl:text-3xl font-bold">CareCamp</span>
+                <span className="font-poppins max-[275px]:text-sm min-[400px]:text-lg sm:text-xl lg:text-2xl xl:text-3xl font-bold">CareCamp</span>
             </Link>
             <div className="drawer lg:drawer-open">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content w-11/12 mx-auto">
                     {/* Page content here */}
-                    <div className="flex max-[249px]:flex-col-reverse justify-end items-center gap-1 sm:gap-2 lg:gap-3 xl:gap-4 py-3 min-[300px]:py-2 min-[400px]:py-3 sm:py-4">
+                    <div className="flex max-[215px]:flex-col-reverse justify-end max-[215px]:items-end items-center gap-2 lg:gap-3 xl:gap-4 py-3 min-[300px]:py-2 min-[400px]:py-3 sm:py-4">
                         <ThemeToggler></ThemeToggler>
                         <label htmlFor="my-drawer-2" className="drawer-button lg:hidden">
                             <GiHamburgerMenu className="min-[300px]:text-lg min-[350px]:text-xl sm:text-2xl" />
@@ -34,57 +56,64 @@ const DashboardLayout = () => {
                     <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
                     <ul className="bg-black text-white text-xs min-[276px]:text-sm min-[400px]:text-base lg:text-lg min-h-full w-max lg:w-80 px-2 lg:px-6 pt-14 min-[400px]:pt-16 sm:pt-20 lg:pt-24 xl:pt-28 pb-4 lg:pb-6 flex flex-col justify-between">
                         {/* Sidebar content here */}
-                        <div className="space-y-3">
+                        <div className="space-y-5">
                             <li>
-                                <NavLink to="/dashboard/profile" className={({isActive})=>isActive? "flex items-center gap-2 text-primary bg-white px-2 rounded-md": "flex items-center gap-2 hover:bg-primary px-2 rounded-md"}>
+                                <NavLink to="/dashboard" className={({ isActive }) => isActive ? "flex items-center gap-2 text-primary bg-white px-2 rounded-md" : "flex items-center gap-2 hover:bg-primary px-2 rounded-md"}>
                                     <FaRegUserCircle />
                                     Profile
                                 </NavLink>
                             </li>
-                            <li>
-                                <NavLink to="/dashboard/add-camp" className={({ isActive }) => isActive ? "flex items-center gap-2 text-primary bg-white px-2 rounded-md" : "flex items-center gap-2 hover:bg-primary px-2 rounded-md"}>
-                                    <MdAddHome />
-                                    Add a Camp
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/manage-camps" className={({ isActive }) => isActive ? "flex items-center gap-2 text-primary bg-white px-2 rounded-md" : "flex items-center gap-2 hover:bg-primary px-2 rounded-md"}>
-                                    <RiHomeGearLine />
-                                    Manage Camps
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/manage-registered-camps" className={({isActive})=>isActive? "flex items-center gap-2 text-primary bg-white px-2 rounded-md": "flex items-center gap-2 hover:bg-primary px-2 rounded-md"}>
-                                    <RiHomeGearFill />
-                                    Manage Registered Camps
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/manage-participants" className={({isActive})=>isActive? "flex items-center gap-2 text-primary bg-white px-2 rounded-md": "flex items-center gap-2 hover:bg-primary px-2 rounded-md"}>
-                                    <MdManageAccounts />
-                                    Manage Participants
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/registered-camps" className={({isActive})=>isActive? "flex items-center gap-2 text-primary bg-white px-2 rounded-md": "flex items-center gap-2 hover:bg-primary px-2 rounded-md"}>
-                                    <FaHouseMedicalCircleCheck />
-                                    Registered Camps
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/payment-history" className={({isActive})=>isActive? "flex items-center gap-2 text-primary bg-white px-2 rounded-md": "flex items-center gap-2 hover:bg-primary px-2 rounded-md"}>
-                                    <FaHistory />
-                                    Payment History
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="/dashboard/analytics" className={({isActive})=>isActive? "flex items-center gap-2 text-primary bg-white px-2 rounded-md": "flex items-center gap-2 hover:bg-primary px-2 rounded-md"}>
-                                    <IoMdAnalytics />
-                                    Analytics
-                                </NavLink>
-                            </li>
+                            {
+                                isAdmin ?
+                                    <>
+                                        <li>
+                                            <NavLink to="/dashboard/add-camp" className={({ isActive }) => isActive ? "flex items-center gap-2 text-primary bg-white px-2 rounded-md" : "flex items-center gap-2 hover:bg-primary px-2 rounded-md"}>
+                                                <MdAddHome />
+                                                Add a Camp
+                                            </NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink to="/dashboard/manage-camps" className={({ isActive }) => isActive ? "flex items-center gap-2 text-primary bg-white px-2 rounded-md" : "flex items-center gap-2 hover:bg-primary px-2 rounded-md"}>
+                                                <RiHomeGearLine />
+                                                Manage Camps
+                                            </NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink to="/dashboard/manage-registered-camps" className={({ isActive }) => isActive ? "flex items-center gap-2 text-primary bg-white px-2 rounded-md" : "flex items-center gap-2 hover:bg-primary px-2 rounded-md"}>
+                                                <RiHomeGearFill />
+                                                Manage Registered Camps
+                                            </NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink to="/dashboard/manage-participants" className={({ isActive }) => isActive ? "flex items-center gap-2 text-primary bg-white px-2 rounded-md" : "flex items-center gap-2 hover:bg-primary px-2 rounded-md"}>
+                                                <MdManageAccounts />
+                                                Manage Participants
+                                            </NavLink>
+                                        </li>
+                                    </> :
+                                    <>
+                                        <li>
+                                            <NavLink to="/dashboard/registered-camps" className={({ isActive }) => isActive ? "flex items-center gap-2 text-primary bg-white px-2 rounded-md" : "flex items-center gap-2 hover:bg-primary px-2 rounded-md"}>
+                                                <FaHouseMedicalCircleCheck />
+                                                Registered Camps
+                                            </NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink to="/dashboard/payment-history" className={({ isActive }) => isActive ? "flex items-center gap-2 text-primary bg-white px-2 rounded-md" : "flex items-center gap-2 hover:bg-primary px-2 rounded-md"}>
+                                                <FaHistory />
+                                                Payment History
+                                            </NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink to="/dashboard/analytics" className={({ isActive }) => isActive ? "flex items-center gap-2 text-primary bg-white px-2 rounded-md" : "flex items-center gap-2 hover:bg-primary px-2 rounded-md"}>
+                                                <IoMdAnalytics />
+                                                Analytics
+                                            </NavLink>
+                                        </li>
+                                    </>
+                            }
                         </div>
-                        <li className="w-max flex items-center gap-2 px-2 hover:text-red-600 cursor-pointer">
+                        <li onClick={handleLogOut} className="w-max flex items-center gap-2 px-2 hover:text-red-600 cursor-pointer">
                             <RiLogoutCircleLine />
                             Log Out
                         </li>
--- src/pages/PrivatePages/SharedPages/Profile.jsx
+++ src/pages/PrivatePages/SharedPages/Profile.jsx
@@ -1,9 +1,39 @@
+import useAuth from "../../../hooks/useAuth";
 
 
 const Profile = () => {
+
+    const {user} = useAuth();
+    const {photoURL: photo, displayName: name, email} = user;
+
     return (
-        <div>
-            USER PROFILE
+        <div className="max-[215px]:min-h-[calc(100vh-120px)] min-[216px]:min-h-[calc(100vh-80px)] min-[300px]:min-h-[calc(100vh-68px)] min-[350px]:min-h-[calc(100vh-72px)] min-[400px]:min-h-[calc(100vh-88px)]  sm:min-h-[calc(100vh-112px)] flex flex-col justify-center items-center">
+            <div className="w-max mx-auto font-bold">
+                <h2 className="w-max text-center text-primary text-3xl">Welcome, {name.split(' ')[0]}!</h2>
+            </div>
+            <div className="w-full min-[400px]:w-11/12 sm:w-1/2 mx-auto flex flex-col justify-center items-center gap-8 p-8 border-2 border-primary rounded-lg shadow-xl bg-white">
+                <div className="w-36 h-36 mx-auto border-2 border-gray-200">
+                    <img src={photo} alt="user_img" className="w-full h-full"/>
+                </div>
+                <div className="w-full flex flex-col sm:flex-row justify-between items-center">
+                    <div className="flex flex-col items-center lg:text-xl">
+                        <p className="w-max text-center text-black/70">Name</p>
+                        <p className="w-max text-center text-black font-semibold">{name}</p>
+                    </div>
+                    <div className="flex flex-col items-center lg:text-xl">
+                        <p className="w-max text-center text-black/70">Email</p>
+                        <p className="w-max text-center text-black font-semibold">{email}</p>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center gap-2">
+                    <button className="w-max px-3 py-1 bg-green-500 text-white rounded-xl outline-none hover:scale-105">
+                        Update Profile
+                    </button>
+                    <button className="w-max px-3 py-1 bg-green-500 text-white rounded-xl outline-none hover:scale-105">
+                        Reset Password
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
